095b80cd096fa075c5f5309bdf9cdab2c69a9609
AbdelazizMG <68661639+AbdelazizMG@users.noreply.github.com>
2025-02-21 10:36:59 +0200

    Add Updates on the backend

--- Backend/ecommerce/myapp/urls.py
+++ Backend/ecommerce/myapp/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("add/",views.add_person, name ="add"),
-    path("show/",views.show_person, name ="show"),
-    path("api/Categorysearch/",views.Categorysearch_view, name ="show"),
     path("api/Homepage/",views.Homepage, name ="search"),
     path("api/AIsearch/",views.AIsearch_view,name="search"),
     path("api/Normalsearch/",views.Normalsearch_view,name="search"),
--- Backend/ecommerce/myapp/views.py
+++ Backend/ecommerce/myapp/views.py
@@ -2,33 +2,14 @@ from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from bson import ObjectId
 from .models import test_collection
-import json, random
+import json, random, re
 from query_translator import QueryTranslator
 
-def home(request):
-    pass
-
-def add_person(request):
-    record : dict = {
-        'name' : 'zezo',
-        'price' : 123
-    }
-
-    test_collection.insert_one(record)
-    return HttpResponse(f"{record}")
-
-def show_person(request):
-    temp = test_collection.find({ "price": { "$lt": 500 } })
-    # temp = test_collection.find({ "price": { "$eq": 150 } })
-    # temp = test_collection.find({ "price": { "$ne": 150 } })
-    # temp = test_collection.find({ "price": { "$lte": 150 } })
-    # temp = test_collection.find({ "price": { "$gt": 150 } })
-    # temp = test_collection.find({ "price": { "$gte": 150 } })
-    # temp = test_collection.find({ "price": { "$in": [100, 150, 200] } })
-    # temp = test_collection.find({ "price": { "$nin": [100, 150] } })
-    return HttpResponse(temp)
-
 # Create your views here.
+
+def home(request) -> JsonResponse :
+    return JsonResponse({}, status=200)
+
 def Homepage(request)  -> JsonResponse:
     '''
     Function to search for 20 random products at the homepage of the website
@@ -80,12 +61,24 @@ def AIsearch_view(request) -> JsonResponse:
             QueryTranslator_obj = QueryTranslator()
             QueryTranslator_obj.test_connection()
             query = QueryTranslator_obj.execute_query(query)
+            print(f"Before the load line {query} {type(query)}")
+            query = query.replace("\\","")   #Solve the issue of back slash
+            pattern = r'(?<!")(\$[a-zA-Z0-9]+)(?!")'
+    
+            # Add double quotes around found MongoDB operators
+            query = re.sub(pattern, r'"\1"', query)
+
+            print(f"After the replacement {query}")
             query_json = json.loads(query)
+            print('##############################################')
             print(query_json)
             print('##############################################')
             #temp = list(test_collection.find({ "description": { "$regex": query, "$options": "i" } }))
             temp = test_collection.find(query_json)
 
+            # if len(list(temp)) > 100 :
+            #     temp = temp[0:100]
+            #     print("Here in the length modification")
             # Convert ObjectIds to strings (important!)
             results = []
             for item in temp:
@@ -152,40 +145,3 @@ def Normalsearch_view(request) -> JsonResponse:
     
     else:
         return JsonResponse({"error": "Invalid request method"}, status=405)
-    
-def Categorysearch_view(request) -> JsonResponse:
-    '''
-    Function to search using the search bar
-    '''
-    if request.method == "GET":
-        try:
-            query = request.GET.get("query", "")
-
-            if not query:
-                return JsonResponse({"error": "Query parameter is missing"}, status=400)
-
-            print("User searched for:", query, type(query))  # Debugging output
-
-            # Return Filtered products based on search
-            temp = list(test_collection.find({ "category": { "$regex": query, "$options": "i" } }))
-
-            #print(temp)
-            # Convert ObjectIds to strings (important!)
-            results = []
-            for item in temp:
-                item_dict = {}  # Create a dictionary for each item
-                for key, value in item.items():
-                    if isinstance(value, ObjectId):  # Check if value is an ObjectId
-                        item_dict[key] = str(value)   # Convert ObjectId to string
-                    else:
-                        item_dict[key] = value
-                results.append(item_dict)
-
-            print(results)
-            return JsonResponse(results, safe=False) # safe=False because you're serializing a list 
-        
-        except json.JSONDecodeError:
-            return JsonResponse({"error": "Invalid JSON format"}, status=400)
-    
-    else:
-        return JsonResponse({"error": "Invalid request method"}, status=405)    
